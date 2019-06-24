b7a2f99c9afc6010b1c5c5c2aeb8937732ef1637
Angela Lo <angelawenlo@gmail.com>
2019-06-24 11:25:51 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -27,7 +27,7 @@ class CashRegister
   end
 
   def void_last_transaction
-    self.total = self.total - self.last_transaction
+    self.total = self.total - self.last_transaction.to_f
     self.items = self.items.pop
   end
 
